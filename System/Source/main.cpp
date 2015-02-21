a85a1a8b3b64180f081f2a2f7147e53c8f4f10b0
Francis Dube <francis-2.dube@polymtl.ca>
2015-02-21 14:09:46 -0500

    Ajout des planetes

--- System/Source/main.cpp
+++ System/Source/main.cpp
@@ -1,31 +1,12 @@
-// Prénoms, noms et matricule des membres de l'équipe:
-// - Prénom1 NOM1 (matricule1)
-// - Prénom2 NOM2 (matricule2)
-
 #include <iostream>
 #include <GL/glut.h>
+
+#include "../Header/Planet.h"
 #include "../Header/varglob.h"
 #include "../Header/float3.h"
 
-#if 1
-// pour un W
-float p1[3] = { -4.0,  2.0,  0.0 };
-float p2[3] = { -3.0, -3.0,  0.0 };
-float p3[3] = { -1.0, -3.0,  0.0 };
-float p4[3] = {  0.0,  0.0,  0.0 };
-float p5[3] = {  1.0, -3.0,  0.0 };
-float p6[3] = {  3.0, -3.0,  0.0 };
-float p7[3] = {  4.0,  2.0,  0.0 };
-#else
-// pour une flèche (Voir apprentissage supplémentaire)
-float p1[3] = { -3.0,  1.0,  0.0 };
-float p2[3] = { -3.0, -1.0,  0.0 };
-float p3[3] = {  0.0, -1.0,  0.0 };
-float p4[3] = { -0.5, -2.5,  0.0 };
-float p5[3] = {  3.0,  0.0,  0.0 };
-float p6[3] = { -0.5,  2.5,  0.0 };
-float p7[3] = {  0.0,  1.0,  0.0 };
-#endif
+//pointeur vers le modele
+GLUquadric *sphere = NULL;
 
 void initialisation()
 {
@@ -39,145 +20,30 @@ void initialisation()
    glMatrixMode( GL_MODELVIEW );
    glLoadIdentity();
 
+   //pointe vers le modele de la sphere
+   sphere = gluNewQuadric();
+
    // activer le mélange de couleur pour bien voir les possibles plis à l'affichage
    glEnable( GL_BLEND );
    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+}
+
+void afficherPlanete(Planet& planete){
+	glColor3f(planete.color[0], planete.color[1], planete.color[2]);
+	gluSphere(sphere, planete.rayon, 16, 16);
 }
 
 void afficherScene()
 {
    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
-   //premiere ligne
-   glColor3f(1.0, 1.0, 1.0);		//couleur blanche
-   glLoadIdentity();				//position
-   glBegin(GL_LINES);
-	   //ligne 1
-	   glVertex3f(-12, 0.0, 0);
-	   glVertex3f(12, 0.0, 0);
-	   //ligne 2
-	   glVertex3f(-4.0, 8.0, 0);
-	   glVertex3f(-4.0, -8.0, 0); 
-	   //ligne 3
-	   glVertex3f(4.0, 8.0, 0);
-	   glVertex3f(4.0, -8.0, 0);
-   glEnd();
+   Planet planeteTest;
+   afficherPlanete(planeteTest);
+
+
+
 					
-   glColor4f(1, 0, 0,0.8);			// utiliser du rouge
-   glTranslatef(-8.0, 4.0, 0);		//position 
-   glBegin(GL_QUADS);
-		//quad 1
-		glVertex3fv(p1);
-		glVertex3fv(p2);
-		glVertex3fv(p3);
-		glVertex3fv(p4);
-		//quad 2
-		glVertex3fv(p4);
-		glVertex3fv(p5);
-		glVertex3fv(p6);
-		glVertex3fv(p7);
-		//quad 3
-		glVertex3fv(p1);
-		glVertex3fv(p4);
-		glVertex3fv(p7);
-		glVertex3fv(p1);  
-   glEnd();
 
-   glColor4f(0,1,0,0.8);			// utiliser du vert
-   glTranslatef(8, 0, 0);		//position 
-   glBegin(GL_TRIANGLES);
-		//triangle 1
-		glVertex3fv(p1);
-		glVertex3fv(p2);
-		glVertex3fv(p3);
-		//triangle 2
-		glVertex3fv(p1);
-		glVertex3fv(p3);
-		glVertex3fv(p4);
-		//triangle 3
-		glVertex3fv(p1);
-		glVertex3fv(p4);
-		glVertex3fv(p7);
-		//triangle 4
-		glVertex3fv(p4);
-		glVertex3fv(p5);
-		glVertex3fv(p7);
-		//triangle 5
-		glVertex3fv(p7);
-		glVertex3fv(p5);
-		glVertex3fv(p6);
-   glEnd();
-
-   glColor4f(0, 0, 1, 0.8);		// utiliser du bleu
-   glTranslatef(8, 0, 0);		//position 
-   glBegin(GL_POLYGON);
-	   //polygone1 (doit etre convexe)
-	   glVertex3fv(p4);			//point duquel partent tout les triangles formant le polygone
-								//(NE PAS UTILISER CETTE OPTIMISATION, PAS GARANTIE)
-	   glVertex3fv(p3);
-	   glVertex3fv(p2);
-	   glVertex3fv(p1);
-	   glVertex3fv(p7);
-	   glVertex3fv(p6);
-	   glVertex3fv(p5);
-   glEnd();
-   
-   glColor4f(1, 1, 0, 0.8);		// utiliser du jaune
-   glTranslatef(0, -8.0, 0);	//position 
-   glBegin(GL_QUAD_STRIP);
-		//quad 1
-		glVertex3fv(p2);
-		glVertex3fv(p3);
-		glVertex3fv(p1);
-		glVertex3fv(p4);
-		//quad 2
-		glVertex3fv(p7);
-		glVertex3fv(p4);
-		//quad 3
-		glVertex3fv(p6);
-		glVertex3fv(p5);
-   glEnd();
-
-   glColor4f(1, 0, 1, 0.8);		// utiliser du rose
-   glTranslatef(-8, 0, 0);	//position 
-   glBegin(GL_TRIANGLE_STRIP);
-		//triangle 1
-		glVertex3fv(p2);
-		glVertex3fv(p3);
-		glVertex3fv(p1);
-		//triangle 2
-		glVertex3fv(p4);
-		//triangle 3
-		glVertex3fv(p7);
-		//triangle 3
-		glVertex3fv(p5);
-		//triangle 3
-		glVertex3fv(p6);
-   glEnd();
-
-   glColor4f(0, 1, 1, 0.8);		// utiliser du bleu ciel
-   glTranslatef(-8, 0, 0);		//position 
-   glBegin(GL_TRIANGLE_FAN);
-	   glVertex3fv(p4);			//point duquel partent tout les triangles formant la fan
-	   glVertex3fv(p3);
-	   glVertex3fv(p2);
-	   glVertex3fv(p1);
-	   glVertex3fv(p7);
-	   glVertex3fv(p6);
-	   glVertex3fv(p5);
-   glEnd();
-
-
-   //3 point alignes
-   glColor4f(1, 0.5, 0, 0.8);		// utiliser du vert
-   glLoadIdentity();
-   glTranslatef(0, 0.5, 0);			//position 
-   glBegin(GL_TRIANGLES);
-	   //triangle 1
-	   glVertex3f(-3,0,0);
-	   glVertex3f(0,0,0);
-	   glVertex3f(3,0,0);
-   glEnd();
 
    glutSwapBuffers();
 }
